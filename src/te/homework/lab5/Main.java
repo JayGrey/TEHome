cf8a86ec035c7017c0d9cd398f6858f65205c0b1
JayGrey <jaygrey@yandex.ru>
2019-11-24 17:16:34 +0200

    rewrite main

--- src/te/homework/lab5/Main.java
+++ src/te/homework/lab5/Main.java
@@ -1,13 +1,8 @@
 package te.homework.lab5;
 
+import java.util.stream.Stream;
+
 public class Main {
-    private static void prettyPrint(
-            String message, Task task, double[] vector, double[][] matrix) {
-        System.out.format("========== [ %s ] ==========%n", message);
-        task.processArray(vector);
-        task.processArray(matrix);
-        System.out.println();
-    }
 
     public static void main(String[] args) {
         DoubleArrayReader reader = new DoubleArrayReaderImpl();
@@ -15,9 +10,11 @@ public class Main {
         double[] lab51Array = reader.readOneDimensionalArray("lab51.txt");
         double[][] lab52Array = reader.readTwoDimensionalArray("lab52.txt");
 
-        // Tasks
-        prettyPrint("task 5.1", new Task1(), lab51Array, lab52Array);
-        prettyPrint("task 5.2", new Task2(), lab51Array, lab52Array);
-        prettyPrint("task 5.3", new Task3(), lab51Array, lab52Array);
+        Stream.of(new Task1(), new Task2(), new Task3()).forEach(t -> {
+            System.out.format("========== [ %s ] ==========%n", t.getClass().getSimpleName());
+            t.processArray(lab51Array);
+            t.processArray(lab52Array);
+            System.out.println();
+        });
     }
 }
