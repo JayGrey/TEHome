3059f1ceddcd598d22430f28ee813aca95697f46
JayGrey <jaygrey@yandex.ru>
2019-11-24 10:53:05 +0200

    main 5.1

--- src/te/homework/lab5/Main.java
+++ src/te/homework/lab5/Main.java
@@ -1,7 +1,21 @@
 package te.homework.lab5;
 
 public class Main {
-    public static void main(String[] args) {
+    private static void prettyPrint(
+            String message, ArrayProcessor processor, double[] vector, double[][] matrix) {
+        System.out.format("========== [ %s ] ==========%n", message);
+        System.out.format("1: %f %n", processor.calculate(vector));
+        System.out.format("2: %f %n", processor.calculate(matrix));
+        System.out.format("==================================%n");
+    }
 
+    public static void main(String[] args) {
+        DoubleArrayReader reader = new DoubleArrayReaderImpl();
+
+        double[] lab51Array = reader.readOneDimensionalArray("lab51.txt");
+        double[][] lab52Array = reader.readTwoDimensionalArray("lab52.txt");
+
+        // Task1
+        prettyPrint("task 5.1", new Task1ArrayProcessor(), lab51Array, lab52Array);
     }
 }
